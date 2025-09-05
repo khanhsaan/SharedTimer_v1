b13558add4145750704785b84047a5f9b581e715
khanhsaan <109020045+khanhsaan@users.noreply.github.com>
2025-09-05 11:25:10 +1000

    Defined washing modes

--- mobile/SharedTimer/app/(tabs)/TimerScreen.tsx
+++ mobile/SharedTimer/app/(tabs)/TimerScreen.tsx
@@ -6,12 +6,49 @@ import { useAuth } from "../hooks/useAuth";
 import { supabase } from "@/lib/supabase";
 
 export function TimerScreen({user}: {user: any}) {
+
     const passedUser = {
         userEmail: "",
         userPassword: "",
         userConfirmPassword: "",
     }
     const {signOutHandle} = useAuth({userEmail: passedUser.userEmail, userPassword: passedUser.userPassword, userConfirmPassword: passedUser.userConfirmPassword});
+
+    // Define washing modes
+    const washingModes = {
+      Cotton: [
+        { label: 'Cold', minutes: 143 },        // 88 + 55
+        { label: '20°C', minutes: 148 },        // 93 + 55
+        { label: '40°C', minutes: 392 },        // 337 + 55
+        { label: '60°C', minutes: 350 },        // 295 + 55
+        { label: '90°C', minutes: 204 },        // 149 + 55
+      ],
+      Mix: [
+        { label: 'Cold', minutes: 121 },        // 66 + 55
+        { label: '40°C', minutes: 133 },        // 78 + 55
+        { label: '60°C', minutes: 150 },        // 95 + 55
+      ],
+      Synthetic: [
+        { label: 'Cold', minutes: 116 },        // 61 + 55
+        { label: '40°C', minutes: 128 },        // 73 + 55
+      ],
+      Delicate: [
+        { label: 'Cold', minutes: 111 },        // 56 + 55
+        { label: '20°C', minutes: 114 },        // 59 + 55
+      ],
+      'Tub Clean': [
+        { label: '60°C', minutes: 133 },        // 78 + 55
+      ],
+      Spin: [
+        { label: 'No temperature', minutes: 69 }, // 14 + 55
+      ],
+      "Quick 15'": [
+        { label: 'Cold', minutes: 70 },         // 15 + 55
+      ],
+      'Rinse+Spin': [
+        { label: 'Cold', minutes: 89 },         // 34 + 55
+      ],
+    };
 
     return (
         <SafeAreaView style={styles.container}>
