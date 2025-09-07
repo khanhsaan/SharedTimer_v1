7973d3bfdc440d14f868cc27825330ce19bdd9b3
khanhsaan <109020045+khanhsaan@users.noreply.github.com>
2025-09-07 17:51:34 +1000

    Create simple mechanism of the timer

--- mobile/SharedTimer/app/hooks/useTimers.ts
+++ mobile/SharedTimer/app/hooks/useTimers.ts
@@ -35,26 +35,44 @@ export function useTimer(initialSeconds = 0){
     }
 
     // Adjust the time (e.g +60 or -30 seconds)
-    const update = useCallback((increase: any) => {
+    const update = ((increase: any) => {
+        // Set the latest value by adding it with the increase value
+        // Math.max(0, ...): Ensures the result never goes below 0 (prevents negative time)
         setRemaining((prev) => Math.max(0, prev + increase));
-    }, []);
+    });
 
-    // Ticking effect
+    // ALTERNATIVE: use the dependency array to cache the latest value of remaining
+    // However, can lead to stale remaining value due to rapid users' clicks, etc. 
+    const update_useCallBack = useCallback((increase: any) => {
+        // Set the latest value by adding it with the increase value
+        // Math.max(0, ...): Ensures the result never goes below 0 (prevents negative time)
+        setRemaining(Math.max(0, remaining + increase));
+    }, [remaining]);
+
+    // Counting down effect
+    // Listen to the state of the timer running
     useEffect(() => {
+        // If the timer is running
         if(running){
+            // Using setInterval() to run setRemaining() every 1 sec
             intervalRef.current = setInterval(() => {
                 setRemaining((prev) => {
+                    // If the value has reached 1 or value being smaller than one
                     if(prev <= 1){
+                        // clear interval
                         if(intervalRef.current) clearInterval(intervalRef.current);
+                        // stop the timer
                         setRunning(false);
+                        // return time of 0
                         return 0;
                     }
-
+                    // Else, keep counting down
                     return prev - 1;
                 });
             }, 1000);
         }
-
+        
+        // If the timer has stopped, clear the interval
         return () => {
             if(intervalRef.current) clearInterval(intervalRef.current);
         }
