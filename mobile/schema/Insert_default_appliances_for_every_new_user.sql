237495bcfad797650b96bcdd4fd65fa90de94ccc
khanhsaan <109020045+khanhsaan@users.noreply.github.com>
2025-09-10 21:16:03 +1000

    Modify triggers

--- mobile/schema/Insert_default_appliances_for_every_new_user.sql
+++ mobile/schema/Insert_default_appliances_for_every_new_user.sql
@@ -1,17 +1,21 @@
--- This function is to insert the default appliances for EVERY new user
 create or replace function create_default_appliances()
-returns trigger as $$
+returns trigger
+language plpgsql
+security definer
+set search_path = public
+as $$
 begin
-insert into appliances (user_id, name, timer) values
-(new.id, 'Washing Machines', 0),
-(new.id, 'Dryer', 7200),
-(new.id, 'Air Fryer', 0),
-(new.id, 'Gas Stove', 0);
-return new;
+  insert into appliances (user_id, name, timer) values
+    (new.id, 'Washing Machine', 0),
+    (new.id, 'Dryer', 7200),
+    (new.id, 'Air Fryer', 0),
+    (new.id, 'Gas Stove', 0);
+  return new;
 end;
-$$ language plpgsql;
+$$;
 
--- This trigger fires on every user creation
+-- drop trigger if exists on_user_created on auth.users;
+
 create trigger on_user_created
 after insert on auth.users
 for each row execute function create_default_appliances();
