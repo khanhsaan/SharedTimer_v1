1828170cbdef29698c7a723336eabad8def67a25
khanhsaan <anthonykhanhan@gmail.com>
2025-10-02 17:53:22 +1000

    Modify useAuth.ts

--- mobile/SharedTimer/app/hooks/useAuth.ts
+++ mobile/SharedTimer/app/hooks/useAuth.ts
@@ -2,53 +2,53 @@ import { useState } from "react";
 import { supabase } from "@/lib/supabase";
 import { useStateForPath } from "@react-navigation/native";
 
-export const useAuth = ({userEmail, userPassword, userConfirmPassword}:{userEmail: string, userPassword: string, userConfirmPassword: string}) => {
-    const[authSignInError, setAuthSignInError] = useState<any>(null);
-    const[authSignInData, setAuthSignInData] = useState<any>(null); // Accept any variable type
-    // store and set the current signed in user
-    const[user, setUser] = useState<any>(null);
+export const useAuth = ({ userEmail, userPassword, userConfirmPassword }: { userEmail: string, userPassword: string, userConfirmPassword: string }) => {
+  const [authSignInError, setAuthSignInError] = useState<any>(null);
+  const [authSignInData, setAuthSignInData] = useState<any>(null); // Accept any variable type
+  // store and set the current signed in user
+  const [user, setUser] = useState<any>(null);
 
-    const[authSignUpError, setAuthSignUpError] = useState<any>(null);
-    const[authSignUpData, setAuthSignUpData] = useState<any>(null); // Accept any variable type
+  const [authSignUpError, setAuthSignUpError] = useState<any>(null);
+  const [authSignUpData, setAuthSignUpData] = useState<any>(null); // Accept any variable type
 
-    // Sign in handling component
-  const signInHandle = async() => {
+  // Sign in handling component
+  const signInHandle = async () => {
     // Pass userEmail and userPassword to supabase.auth
-    const {data, error} = await supabase.auth.signInWithPassword({
+    const { data, error } = await supabase.auth.signInWithPassword({
       email: userEmail,
       password: userPassword,
     });
 
     // Email and password must not be empty
-    if(!userEmail || !userPassword ) {
-        setAuthSignInError("Email or password is empty");
-        console.warn(authSignInError);
+    if (!userEmail || !userPassword) {
+      setAuthSignInError("Email or password is empty");
+      console.warn(authSignInError);
 
-        return {
-            authSignInData: null,
-            authSignInError: authSignInError
-        };
+      return {
+        authSignInData: null,
+        authSignInError: authSignInError
+      };
     }
 
     // Retrieve the data and error from AuthHandle
     // If there is error, set error message and print it
-    if(error){
-        const code = error.code;
-        setAuthSignInError(code);
-        console.error(`Sign-in FAILED!`, code);
+    if (error) {
+      const code = error.code;
+      setAuthSignInError(code);
+      console.error(`Sign-in FAILED!`, code);
 
-        return {
-            authSignInData: null,
-            authSignInError: authSignInError
-        }
-    } 
+      return {
+        authSignInData: null,
+        authSignInError: authSignInError
+      }
+    }
     // Else, the sign in process is SUCCESSFUL, reset the error message, print the data
-    else{
+    else {
       // Reset error message if there is any from the previous stage
       setAuthSignInError('');
       console.log(`Sign-in successfully! `, data);
       setUser(data.user)
-      
+
       return {
         authSignInData: data,
         authSignInError: null
@@ -57,8 +57,8 @@ export const useAuth = ({userEmail, userPassword, userConfirmPassword}:{userEmai
   };
 
   // Sign out handling component
-  const signOutHandle = async() => {
-    const{error} = await supabase.auth.signOut();
+  const signOutHandle = async () => {
+    const { error } = await supabase.auth.signOut();
 
     return {
       error,
@@ -66,76 +66,76 @@ export const useAuth = ({userEmail, userPassword, userConfirmPassword}:{userEmai
   };
 
   // Sign Up handle
-  const signUpHandle = async() => {
-    const{ data, error } = await supabase.auth.signUp({
-        email: userEmail,
-        password: userPassword
+  const signUpHandle = async () => {
+    const { data, error } = await supabase.auth.signUp({
+      email: userEmail,
+      password: userPassword
     });
 
     setAuthSignUpData(data);
     setAuthSignUpError(authSignInError);
 
     // Email and password must not be empty
-    if(!userEmail || !userPassword ) {
-        setAuthSignUpError("Email or password is empty");
-        console.warn(authSignUpError);
-        return (
-            {authSignUpData: null, authSignUpError}
-        );
+    if (!userEmail || !userPassword) {
+      setAuthSignUpError("Email or password is empty");
+      console.warn(authSignUpError);
+      return (
+        { authSignUpData: null, authSignUpError }
+      );
     }
     // Retrieve the data and error from AuthHandle
     // If there is error, set error message and print it
 
-    if(userConfirmPassword !== userPassword){
-        setAuthSignUpError("Confirm password does not match");
-        console.warn(authSignUpError);
-        return (
-            {authSignUpData: null, authSignUpError}
-        );
+    if (userConfirmPassword !== userPassword) {
+      setAuthSignUpError("Confirm password does not match");
+      console.warn(authSignUpError);
+      return (
+        { authSignUpData: null, authSignUpError }
+      );
     }
 
-    if(error){
-        const code = error.code;
-        const name = error.name;
-        // DEBUG
-        console.error(`Sign-up FAILED!`, code);
-        return (
-          {
-            signUpData: null,
-            signUpError: code
-          }
-        )
-    } 
+    if (error) {
+      const code = error.code;
+      const name = error.name;
+      // DEBUG
+      console.error(`Sign-up FAILED!`, code);
+      return (
+        {
+          signUpData: null,
+          signUpError: code
+        }
+      )
+    }
     // Else, the sign in process is successful, reset the error message, print the data
-    else{
-        // Reset error message if there is any from the previous stage
-        setAuthSignUpError('');
-        console.log(`Sign-up successfully! `, data);
-        return (
-            {
-              signUpData: authSignUpData, 
-              signUpError: null
-            }
-        );
+    else {
+      // Reset error message if there is any from the previous stage
+      setAuthSignUpError('');
+      console.log(`Sign-up successfully! `, data);
+      return (
+        {
+          signUpData: authSignUpData,
+          signUpError: null
+        }
+      );
     }
   }
 
   const mapErrorMessage = (error: any) => {
-      const message = (error?.message || error?.error_description || "").toLowerCase();
-      const code  = (error?.code || error?.error_code).toLowerCase();
-      const status = error?.status;
+    const message = (error?.message || error?.error_description || "").toLowerCase();
+    const code = (error?.code || error?.error_code).toLowerCase();
+    const status = error?.status;
 
-      let error_message = "";
+    let error_message = "";
 
-      if(code === "weak_password") {
-        return error_message = "Password must contain atleast 6 characters."
-      }
+    if (code === "weak_password") {
+      return error_message = "Password must contain atleast 6 characters."
+    }
 
-      if(message.includes("already registered") || message.includes("already exists") || code == "user_already_exists" || status === 422){
-        return error_message = "Account has been registered!";
-      } else {
-        return error_message = message;
-      }
+    if (message.includes("already registered") || message.includes("already exists") || code == "user_already_exists" || status === 422) {
+      return error_message = "Account has been registered!";
+    } else {
+      return error_message = message;
+    }
   }
 
   return {
