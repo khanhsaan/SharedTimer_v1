e37500dfd66baa8ed09628713c545d30fc2ea1d1
khanhsaan <anthonykhanhan@gmail.com>
2025-11-29 11:01:04 +1100

    Refactor: Declare Stack screen for (auth)/_layout.tsx

--- mobile/SharedTimer/app/(auth)/_layout.tsx
+++ mobile/SharedTimer/app/(auth)/_layout.tsx
@@ -1,3 +1,10 @@
-export default function RootLayout() {
-    
+import { Stack } from "expo-router";
+
+export default function AuthLayout() {
+    return (
+        <Stack screenOptions={{ headerShown: false}}>
+            <Stack.Screen name="AuthScreen"></Stack.Screen>
+            <Stack.Screen name="ProfileGate"></Stack.Screen>
+        </Stack>
+    )
 }
