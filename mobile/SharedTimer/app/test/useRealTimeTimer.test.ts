e5609067f58679828ca34e9b73bbd89783a0b90d
khanhsaan <anthonykhanhan@gmail.com>
2026-02-07 18:59:02 +1100

    refactor

---
+++ mobile/SharedTimer/app/test/useRealTimeTimer.test.ts
@@ -0,0 +1,10 @@
+import useRealTimeTimer from "../hooks/useRealTimeTimer"
+import {setTimerValue} from useRealTimeTimer;
+
+describe('setTimerValue', () => {
+    it('should set timer remaining, base time, start hour & finish hour with corresponding passed value in sec', () => {
+        const applianceID = 'washingMachine';
+        const value = 3000;
+        const result = setTimerValue(applianceID, value);
+    })
+})
